97ddca4deae3ba3146ee2b480ee4be630f614a4f
BallerJColt <koltai.b@gmail.com>
2019-03-04 15:22:58 +0100

    Created helper class to get possible portal exit directions, added then reverted change to generate seeded maze sequence, will implement properly tomorrow. Added a ton of comments

--- MazeGeneration/Assets/Scripts/MapGenerator.cs
+++ MazeGeneration/Assets/Scripts/MapGenerator.cs
@@ -13,22 +13,40 @@ public class MapGenerator : MonoBehaviour
     {
         InitializeMazes();
     }
-    
-    void Update()
-    {
-
-    }
 
     void InitializeMazes()
     {
         for (int i = 0; i < mazeCount; i++)
         {
-            Vector3 mazeSpawnPoint = new Vector3(transform.position.x + i * (mazeRows + 1), 0, 0);
+            Vector3 mazeSpawnPoint = new Vector3(transform.position.x + i * (mazeCols + 1), 0, 0);
             GameObject tempMaze = Instantiate(mazeGeneratorPrefab, mazeSpawnPoint, Quaternion.identity);
             tempMaze.name = "Maze " + i.ToString();
             tempMaze.transform.parent = transform;
             MazeGenerator mazeScript = tempMaze.GetComponent<MazeGenerator>();
             mazeScript.SetDimensions(mazeRows, mazeCols);
+            mazeScript.InitializeMaze();
+            mazeScript.GenerateRandomMaze();
+
         }
+    }
+
+    private int[] FindFirstDeadEnd(MazeGenerator maze, int rowFlag, int colFlag)
+    {
+        for (int i = 0; i < mazeRows; i++)
+        {
+            for (int j = 0; j < mazeCols; j++)
+            {
+                if (i == rowFlag && j == colFlag) // skip previous entrance
+                    continue;
+                if (maze.mazeIntArray[i, j] == 1 || maze.mazeIntArray[i, j] == 2 || maze.mazeIntArray[i, j] == 4 || maze.mazeIntArray[i, j] == 8)
+                    return new int[] { i, j };
+            }
+        }
+        return new int[] { 0, 0 };
+    }
+
+    private int[] FindFirstDeadEnd(MazeGenerator maze)
+    {
+        return FindFirstDeadEnd(maze, -1, -1);
     }
 }
--- MazeGeneration/Assets/Scripts/MazeGenerator.cs
+++ MazeGeneration/Assets/Scripts/MazeGenerator.cs
@@ -11,16 +11,17 @@ public class MazeGenerator : MonoBehaviour
     bool matCheck = false;
     public int[,] mazeIntArray;
 
-    // Start is called before the first frame update
-    void Start()
+    // Staart is called before the first frame update
+    /* void Start()
     {
         InitializeMaze();
-        GenerateMaze();
-        //SeededMazeGeneration(0,0,1); // now we seeded
+        //GenerateRandomMaze();
+        //GenerateSeededMaze(0,0,1); // now we seeded
+        //GenerateSeededMaze(0, 0, 1, mazeRows - 1, mazeColumns - 1, 0);
         GenerateIntArray();
-    }
+    } */
 
-    // Update is called once per frame
+    // The inputs here are just for debug reasons to show the wang tiles
     void Update()
     {
         if (Input.GetKeyUp("space"))
@@ -43,7 +44,7 @@ public class MazeGenerator : MonoBehaviour
         }
     }
 
-    void InitializeMaze()
+    public void InitializeMaze()
     {
         tileArray = new Tile[mazeRows, mazeColumns];
         //float mazeHalfWidth = mazeRows / 2f; // Add scalability with tile width!
@@ -61,13 +62,15 @@ public class MazeGenerator : MonoBehaviour
         }
     }
 
-    void GenerateMaze()
+    // Random starting positions
+    public void GenerateRandomMaze()
     {
         int startRow = Random.Range(0, mazeRows);
         int startCol = Random.Range(0, mazeColumns);
         RecursiveDFS(startRow, startCol);
     }
 
+    //Generates an integer array with the tileIDs in it. Will be used to find dead ends for portal placement
     void GenerateIntArray()
     {
         mazeIntArray = new int[mazeRows, mazeColumns];
@@ -80,25 +83,55 @@ public class MazeGenerator : MonoBehaviour
         }
     }
 
-    void SeededMazeGeneration(int startRow, int startCol, int startDirection)
+    // Maze generation using recursive DFS with a starting position and direction as seed.
+    // It makes the first connection manually, then calls the RecursiveDFS() method to generate the rest of the maze.
+    public void GenerateSeededMaze(int startRow, int startCol, int startDirection)
     {
         switch (startDirection)
         {
             case 0:
-                Tile.ConnectTiles(tileArray[startRow, startCol], tileArray[startRow - 1, startCol], 0);
+                Tile.ConnectTiles(tileArray[startRow, startCol], tileArray[startRow - 1, startCol], startDirection);
                 RecursiveDFS(startRow - 1, startCol);
                 break;
             case 1:
-                Tile.ConnectTiles(tileArray[startRow, startCol], tileArray[startRow, startCol + 1], 1);
+                Tile.ConnectTiles(tileArray[startRow, startCol], tileArray[startRow, startCol + 1], startDirection);
                 RecursiveDFS(startRow, startCol + 1);
                 break;
             case 2:
-                Tile.ConnectTiles(tileArray[startRow, startCol], tileArray[startRow - 1, startCol], 2);
+                Tile.ConnectTiles(tileArray[startRow, startCol], tileArray[startRow + 1, startCol], startDirection);
                 RecursiveDFS(startRow + 1, startCol);
                 break;
             case 3:
-                Tile.ConnectTiles(tileArray[startRow, startCol], tileArray[startRow - 1, startCol], 3);
+                Tile.ConnectTiles(tileArray[startRow, startCol], tileArray[startRow, startCol - 1], startDirection);
                 RecursiveDFS(startRow, startCol - 1);
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Overload of the seeded maze generation method with a specified end position and direction as well.
+    // It opens the end tile so it will be ignored by the generator method, then calls the GenerateSeededMaze(int,int,int) method,
+    // finally it connects the end tile with its neighbour to the specified direction.
+    void GenerateSeededMaze(int startRow, int startCol, int startDirection, int endRow, int endCol, int endDirection)
+    {
+        tileArray[endRow, endCol].OpenWall(endDirection);
+
+        GenerateSeededMaze(startRow, startCol, startDirection);
+
+        switch (endDirection)
+        {
+            case 0:
+                Tile.ConnectTiles(tileArray[endRow, endCol], tileArray[endRow - 1, endCol], endDirection);
+                break;
+            case 1:
+                Tile.ConnectTiles(tileArray[endRow, endCol], tileArray[endRow, endCol + 1], endDirection);
+                break;
+            case 2:
+                Tile.ConnectTiles(tileArray[endRow, endCol], tileArray[endRow + 1, endCol], endDirection);
+                break;
+            case 3:
+                Tile.ConnectTiles(tileArray[endRow, endCol], tileArray[endRow, endCol - 1], endDirection);
                 break;
             default:
                 break;
@@ -107,6 +140,12 @@ public class MazeGenerator : MonoBehaviour
 
 
     //Probably should be a coroutine as the generation hangs the game on play pretty bad.
+    /*
+    Generates a perfect maze using recursive depth first search. First it gets an array of random directions, then
+    using a switch, it goes through them. In the switch, the method does a bounds check on the neighbour in the specified direction,
+    then checks if it has been visited yet. If not, then it connects the tile to the neighbour, then calls itself (RecursiveDFS(int,int))
+    on the neighbour tile. The method returns when there are no more empty neighbours.
+     */
     void RecursiveDFS(int row, int col)
     {
         int[] directions = GenerateRandomDirections();
@@ -162,6 +201,10 @@ public class MazeGenerator : MonoBehaviour
         mazeColumns = cols;
     }
 
+
+    // This method returns returns a 4 length array with random directions occuring once in each index.
+    // It first creates a base array where the directions are in order, picks one randomly,
+    // puts it into the first index of the resul array and sets it into a -1 "flag" to be ignored in the base array.
     private int[] GenerateRandomDirections()
     {
         int[] baseArray = new int[4] { 0, 1, 2, 3 };
@@ -177,5 +220,24 @@ public class MazeGenerator : MonoBehaviour
             }
         }
         return result;
+    }
+
+    // This method could be used later if we want to simplify the generator methods.
+    // It returns if the neighbour in the specified direction is inbounds or not.
+    private bool IsNeighbourInbounds(int row, int col, int direction)
+    {
+        switch (direction)
+        {
+            case 0:
+                return (row - 1 >= 0);
+            case 1:
+                return (col + 1 <= mazeColumns - 1);
+            case 2:
+                return (row + 1 <= mazeRows - 1);
+            case 3:
+                return (col - 1 >= 0);
+            default:
+                return false;
+        }
     }
 }
